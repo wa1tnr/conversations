b170891e43c2db9373fb52339e971c2836eb3203
wa1tnr <wa1tnr@users.noreply.github.com>
2020-01-22 19:19:23 +0000

    revert changes to reintroduce .. gently

--- arduino/hardware/serpente-core/samd/variants/serpente/variant.h
+++ arduino/hardware/serpente-core/samd/variants/serpente/variant.h
@@ -120,13 +120,7 @@ static const uint8_t A5  = PIN_A5;
 /*
  * SPI Interfaces
  */
-
-// TODO tnr: mention (right here) that metro m0 and feather m0 were conflated by tnr ;)
-// (tnr: have used documentation and code of both, without bothering to refactor)
-// (tnr: oops.)
-
-// TODO tnr: can interfaces count be incremented to '2' here?
-#define SPI_INTERFACES_COUNT 2 // #define SPI_INTERFACES_COUNT 1
+#define SPI_INTERFACES_COUNT 1
 
 #define PIN_SPI_MISO         (2ul) // PA06
 #define PIN_SPI_MOSI         (0ul) // PA04
@@ -135,108 +129,11 @@ static const uint8_t A5  = PIN_A5;
 #define PAD_SPI_TX           SPI_PAD_0_SCK_1
 #define PAD_SPI_RX           SERCOM_RX_PAD_2
 
-// SPI FLASH
-// TODO tnr: review all this as conflation of feather m0 with metro m0 happened
-
-// tnr wanted: correct D-numbers for serpente, here (see CircuitPython stuff).
-// tnr note: serpente-core from technoblogy assumed throughout.
-
-/* circuitpython for serpente follows */
-/*
-from
- circuitpython/ports/atmel-samd/boards/serpente/mpconfigboard.h 
-
-  6 #define SPI_FLASH_MOSI_PIN          &pin_PA16 // tnr D11
-  7 #define SPI_FLASH_MISO_PIN          &pin_PA18 // tnr D12
-  8 #define SPI_FLASH_SCK_PIN           &pin_PA17 // tnr  D9
-  9 #define SPI_FLASH_CS_PIN            &pin_PA15 // tnr D10
-
-from serpente-core technoblogy github repository:
-
- 42   // 9..12 - SPI Flash
- 43   { PORTA, 17, PIO_DIGITAL, PIN_ATTR_DIGITAL, No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXT
- 44   { PORTA, 15, PIO_DIGITAL, PIN_ATTR_DIGITAL, No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXT
- 45   { PORTA, 16, PIO_DIGITAL, PIN_ATTR_DIGITAL, No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXT
- 46   { PORTA, 18, PIO_DIGITAL, PIN_ATTR_DIGITAL, No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXT
-*/
-
-// verified after conflation feather and metro:
-#define PIN_SPI1_MISO        (12ul) //  PA18 // D12  //     PA18 FLASH_MISO // D12
-#define PIN_SPI1_MOSI        (11ul) //  PA16 // D11  //     PA16 FLASH_MOSI // D11
-#define PIN_SPI1_SCK         (9ul)  //  PA17 //  D9  //     PA17 FLASH_SCK  //  D9
-#define PIN_SPI1_SS          (10ul) //  PA15 // D10  //     PA15 FLASH_CS   // D10
-
-// TODO tnr: which sercom is the SPI flashROM on?
-// answer: schematic from arturo182 says 'sercom3 is always spi flash'
-
-// TODO tnr: hint may be ATSAMD21E18A or the like - what did arturo182 use for a chip?
-
-// TODO tnr: What are the names used for SPI flashROM (TX and RX seem likely to be USART stuff)
-
-#define PERIPH_SPI1          sercom3 // serpente r2 schematic
-#define PAD_SPI1_TX          SPI_PAD_0_SCK_1 // tnr checked datasheet e18a: sercom3 pad 0
-#define PAD_SPI1_RX          SERCOM_RX_PAD_2 // tnr checked datasheet e18a: sercom3 pad 2
-
-static const uint8_t SS	  = 3 ; // tnr PA7 ?
+static const uint8_t SS	  = 3 ;
 static const uint8_t MOSI = PIN_SPI_MOSI ;
 static const uint8_t MISO = PIN_SPI_MISO ;
 static const uint8_t SCK  = PIN_SPI_SCK ;
 
-static const uint8_t SS1  = 10 ; // HW SS isn't used.  Set here only for reference. tnr: ?
-// tnr: SS1 = 10 still good.
-static const uint8_t MOSI1 = PIN_SPI_MOSI ; // tnr // odd but metro m0 does this
-static const uint8_t MISO1 = PIN_SPI_MISO ; // tnr // odd but metro m0 does this
-static const uint8_t SCK1  = PIN_SPI_SCK ;  // tnr // odd but metro m0 does this
-
-// notes tnr - all that NO NO NO stuff probably from conflation of
-//  the feather m0 express with the metro m0 express tnr ;)
-
-// factual metro m0 express follows.
-
-// PB03 // D36 // line 94 // SPI Flash MISO // metro m0 only
-// PB22 // D37 // line 95 // SPI Flash MOSI // metro m0 only
-// PB23 // D38 // line 96 // SPI Flash SCK  // metro m0 only
-// PA13 // D39 // line 98 // SPI Flash SS   // metro m0 only
-
-/* adafruit metro m0 stuff follows */
-// variant.cpp metro m0:
-
-/*
- 92   // 36..38 - Secondary SPI
- 93   // ----------------------
- 94   { PORTB, 03, PIO_SERCOM_ALT, PIN_ATTR_NONE, No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXT
- 95   { PORTB, 22, PIO_SERCOM_ALT, PIN_ATTR_NONE, No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXT
- 96   { PORTB, 23, PIO_SERCOM_ALT, PIN_ATTR_NONE, No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXT
- 97   // 39 Secondary SPI SS
- 98   { PORTA, 13, PIO_DIGITAL, PIN_ATTR_DIGITAL, No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXT
-
- 94 No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_NONE },  // SPI Flash MISO
- 95 No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_NONE },  // SPI FLash MOSI
- 96 No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_NONE },  // SPI FLash SCK
- 97 
- 98 No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_13 }, // SPI Flash SS
-*/
-
-/* adafruit metro m0 for circuitpython follows. */
-/*
-
-#define SPI_FLASH_MOSI_PIN          &pin_PB22
-#define SPI_FLASH_MISO_PIN          &pin_PB03
-#define SPI_FLASH_SCK_PIN           &pin_PB23
-#define SPI_FLASH_CS_PIN            &pin_PA13
-
-*/
-
-/* end of adafruit metro m0 for circuitpython - tnr */
-
-/* end of adafruit metro m0 stuff tnr */
-
-// TNR FINAL: 22 Jan Wed 16:43 UTC - everything been gone over, perhaps.
-// should compile or throw comprehensible errors that can be diagnosed.
-
-// ready for a primitive test, perhaps.
-
-// TODO 16:44 UTC 22 Jan - the SPI lib stuff (to exercise the code found here, in variant.h, properly)
 
 /*
  * Wire Interfaces
